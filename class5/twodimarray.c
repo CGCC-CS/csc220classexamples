ba31af93cbb5d13ec4bfaa5a738ceaa49f03605b
Wade Huber <wadehuber@gmail.com>
2022-06-21 20:08:43 -0700

    Summer 2022 example

--- class5/twodimarray.c
+++ class5/twodimarray.c
@@ -1,74 +1,41 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#define ROWS 4
-#define COLS 3
+#define ROWS 3
+#define COLS 7
 
 int main (void) {
 
-    /* Two-dimensional array */
-    char arr1[ROWS][COLS];
+    int * array1D = malloc(COLS * (sizeof(int)));
+    int * array2D[ROWS];
 
-    /* One-dimensional array that we treat like it is 2D */
-    char arr2[ROWS*COLS];
-
-    /* pointer to array of pointers */
-    int ** ptr1;
-    ptr1 = malloc(4 * sizeof (int *));      /* allocates an array of pointers to rows */
-    for(int ii=0;ii<ROWS;ii++){ 
-        ptr1[ii] = malloc(3 * sizeof(int)); /* allocates a row */
+    /* Allocating memory for array2D - allocate each row 
+        each row is a pointer to an array of integers */
+    for(int ii=0;ii<ROWS;ii++) {
+        array2D[ii] = malloc(COLS * (sizeof(int)));
     }
 
-    /* pointer to array of ints */
-    int * ptr2;
-    ptr2 = malloc(ROWS * COLS * sizeof(int));
+    for(int ii=0;ii<COLS;ii++) {
+        array1D[ii] = ii;      /* Can use array indexing for a pointer */
+    }
+    for(int ii=0;ii<ROWS;ii++) {
+        for (int jj=0;jj<COLS;jj++) {
+            array2D[ii][jj] = ii*10 + jj;
+        }
+    }
 
+    printf("array1d: ");
+    for(int ii=0;ii<COLS;ii++) {
+        printf("%d ", *(array1D+ii));  /* Or I can use pointer arithmetic */
+    }
+    printf("\n");
 
-    /* Note that for row r and col c, r * COLS + c is a unique number */
-    
-    /* Initialize & print array 1*/
-    printf("Array 1:\n");
-    for (int ii=0;ii<ROWS;ii++) {
-        for(int jj=0;jj<COLS;jj++) {
-            arr1[ii][jj]='A'+(ii*COLS)+jj;
-            printf("%4c", (arr1[ii][jj]));
+    printf("array2D: \n");
+    for(int ii=0;ii<ROWS;ii++) {
+        for (int jj=0;jj<COLS;jj++) {
+            printf("%3d ", array2D[ii][jj]);
         }
         printf("\n");
     }
-
-    /* Initialize & print array 2*/
-    printf("\n");
-    printf("Array 2:\n");
-    for (int ii=0;ii<ROWS*COLS;ii++) {
-        arr2[ii] = 'N' + ii;
-        printf("%4c", (arr2[ii]));
-        if(((ii+1) % COLS) == 0) {
-            printf("\n");
-        }
-    }
-
-    /* Initialize & print pointer 1*/
-    printf("\n");
-    printf("Pointer 1:\n");
-    for (int ii=0;ii<ROWS;ii++) {
-        for(int jj=0;jj<COLS;jj++) {
-            ptr1[ii][jj]='A'+(ii*COLS)+jj;
-            printf("%4c", (ptr1[ii][jj]));
-        }
-        printf("\n");
-    }
-
-    /* Initialize & print pointer 2*/
-    printf("\n");
-    printf("Pointer 2:\n");
-    for (int ii=0;ii<ROWS*COLS;ii++) {
-        ptr2[ii] = 'N' + ii;
-        printf("%4c", (ptr2[ii]));
-        if(((ii+1) % COLS) == 0) {
-            printf("\n");
-        }
-    }
-
-    printf("\n");
     return 0;
 }
